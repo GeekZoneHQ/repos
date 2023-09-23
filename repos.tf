994ee7e21a2dd756f27585fe0629b94eba894b61
vao11 <victoria.ogundare@crypticshell.co.uk>
2023-09-23 10:26:19 +0100

    added the sample repos

--- repos.tf
+++ repos.tf
@@ -1,1 +1,19 @@
 # List repos here
+
+module "tf_repo1" {
+    source = "./modules/github_repo"
+
+    repo_name = "repo_1"
+    repo_description = "repo 1 description"
+    repo-attributes = var.repo-attributes
+    github_token = local.GITHUB_TOKEN 
+}
+
+module "tf_repo2" {
+    source = "./modules/github_repo"
+
+    repo_name = "repo_2"
+    repo_description = "repo 2 description"
+    repo-attributes = var.repo-attributes
+    github_token = local.GITHUB_TOKEN
+}

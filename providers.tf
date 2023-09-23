b9ea9aff627c1856fbfeaa5294325d6186ac2b35
vao11 <victoria.ogundare@crypticshell.co.uk>
2023-09-23 10:25:03 +0100

    commented out hcp provider and added github provider

--- providers.tf
+++ providers.tf
@@ -1,7 +1,7 @@
-provider "hcp" {
-  # Auth provided by Terraform Cloud env vars
-  project_id = var.hcp_project_id
-}
+# provider "hcp" {
+#   # Auth provided by Terraform Cloud env vars
+#   project_id = var.hcp_project_id
+# }
 
 provider "github" {
   token = local.GITHUB_TOKEN

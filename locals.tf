a994808f542f238815323005e16bc8813efcbcb0
vao11 <victoria.ogundare@crypticshell.co.uk>
2023-09-23 10:18:36 +0100

    commented out hcp secret locals as not using

--- locals.tf
+++ locals.tf
@@ -1,4 +1,8 @@
-locals {
-  github_secrets = data.hcp_vault_secrets_app.github.secrets
-  GITHUB_TOKEN = local.github_secrets["GITHUB_TOKEN"]
+# locals {
+#   github_secrets = data.hcp_vault_secrets_app.github.secrets
+#   GITHUB_TOKEN = local.github_secrets["GITHUB_TOKEN"]
+# }
+
+locals{
+  GITHUB_TOKEN = var.github_token
 }

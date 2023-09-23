ca16ab75bdbeb754523d155fa01296b512f39ee7
vao11 <victoria.ogundare@crypticshell.co.uk>
2023-09-23 10:38:24 +0100

    cleaned up root vars.tf

--- vars.tf
+++ vars.tf
@@ -1,8 +1,34 @@
 variable "org" {
-  description = "The name of our organisation"
-  default = "geekzone"
+  type = string
+  description = "The name of the Github organisation"
+  default = ""
 }
 
-variable "hcp_project_id" {
-  description = "The ID of this project in HCP"
+# variable "hcp_project_id" {
+#   description = "The ID of this project in HCP"
+# }
+
+variable "github_token" {
+  # Github PAT Token
+  type        = string
+  description = "This is the Github Repository or organisation secret for authentication"
+  default     = ""
+}
+ variable "repo-attributes" {
+  type = map(string)
+  description = "All Geek Zone Repos need the follow attributes by default"
+  default = {
+    "visibility" = "public"
+    "has_issues" = true
+    "has_projects" = true
+    "has_wiki" = false
+    "delete_branch_on_merge" = true
+    "auto_init" = true
+    "license_template" = "gpl-3.0"
+    "archive_on_destroy" = false
+    "template_owner" = "GeekZoneHQ"
+    "template_repository" = "Template"
+    "template_include_all_branches" = true
+  }
+  
 }
